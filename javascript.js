f7a582ff172568a578d774ab6c5aea585c03bb2a
bolu3 <coutodiego.f@gmail.com>
2022-06-27 23:12:28 -0300

    Creation of PlayRound function with insensitive cases

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
 function computerPlay() {
-  choices = ["Rock", "Paper", "Scissors"];
-  computerChoice = choices[Math.floor(Math.random() * 3)];
+  let choices = ["ROCK", "PAPER", "SCISSORS"];
+  let computerChoice = choices[Math.floor(Math.random() * 3)];
   console.log(computerChoice);
+  return computerChoice;
+}
+
+playerSelection = "roCK";
+
+function playRound(playerSel, computerPlay) {
+  const a = playerSel.toUpperCase();
+  console.log(a);
 }
 
 computerPlay();
+playRound(playerSelection, computerPlay());
